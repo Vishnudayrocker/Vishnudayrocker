ed6fa125b1f7cf86d23dc6df39205ced5a781b63
Vishnudayrocker <110530712+Vishnudayrocker@users.noreply.github.com>
2022-09-27 10:44:31 +0530

    Add files via upload

---
+++ robb.py
@@ -0,0 +1,37 @@
+lst = []
+  
+n = int(input("Enter number of elements : "))
+  
+for i in range(0, n):
+    e = int(input())
+    lst.append(e) 
+print(lst)
+
+max_1=max(lst)
+
+
+
+if n==3:
+  print("the possible outcomes to rob is",max_1)
+else:
+  print("")  
+
+
+if n==4:
+   if lst.index(max_1)==0:
+     print("the possible outcomes are",max_1+lst[2])
+   elif lst.index(max_1)==1:
+    print("the possible outcomes are",max_1+lst[3])
+   elif lst.index(max_1)==2:
+    print("the possible outcomes are",max_1+lst[0])
+   elif lst.index(max_1)==3:
+    print("the possible outcomes are",max_1+lst[1])
+   else:
+    print("no possible outcomes")
+    
+    
+
+     
+
+
+
